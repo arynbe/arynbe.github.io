e68384784ee8d6e5101f395f832c1df84b64c031
jasiostefanski <jasiostefanski@gmail.com>
2024-10-29 16:58:03 -0500

    speed + 13

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function changeImage() {
     imageElement.src = images[currentIndex];
 }
 
-setInterval(changeImage, 350); 
+setInterval(changeImage, 300); 
 
 // Grab references to the button and div
 const fadeButton = document.getElementById('fadeButton');
